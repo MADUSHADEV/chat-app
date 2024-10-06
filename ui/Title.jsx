7f686cd35c61c1ddfeca72d4f94658c622737e6b
GAYAMINA <gayamina11122@gmail.com>
2024-10-06 23:44:59 +0530

    Update : Create Title

---
+++ ui/Title.jsx
@@ -0,0 +1,28 @@
+import {StyleSheet, Text, View} from "react-native";
+
+// props.title => Title
+// props.size => Font size
+// props.color => Font color
+export function Title(props) {
+    return (
+        <View style={styles.titleView}>
+            <Text style={styles.titleText(props)}>{props.title}</Text>
+        </View>
+    );
+} 
+
+const styles = StyleSheet.create({
+    titleView: {
+        justifyContent:"center",
+        alignItems:"center",
+        padding:5,
+    },
+
+    titleText: (props) => {
+        return {
+            fontSize: props.size || 25,
+            color:props.color || "#000E08",
+            fontWeight:"bold",
+        }   
+    },
+});
