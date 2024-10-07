476c3441c7a4a6afdc255685d90ab25560f77363
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2024-10-07 15:59:38 +0530

    update: LineInput added

--- App.jsx
+++ App.jsx
@@ -1,14 +1,28 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import CustomButton from "./ui/Button";
+import LineInput from "./ui/LineInput";
 import { BUTTON } from "./constants/constants";
 
 const screen = () => {
+  const [test, setTest] = useState("test");
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.buttonDiv}>
-        <CustomButton buttonName={BUTTON.signIn} backgroundColor={"#24786D"} />
+        <LineInput
+          inputLabel={"Lable Name"}
+          placeholder={"Type your dirty secrets..."}
+          onChange={setTest}
+        ></LineInput>
+
+        <Button
+          title="Step On me daddy 💦"
+          onPress={() => {
+            Alert.alert("You Noty 😈", test);
+          }}
+        ></Button>
       </View>
     </SafeAreaView>
   );
@@ -17,7 +31,7 @@ const screen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "black",
+    backgroundColor: "white",
     alignItems: "center",
     justifyContent: "center",
   },
@@ -26,9 +40,10 @@ const styles = StyleSheet.create({
     fontSize: 20,
   },
   buttonDiv: {
+    width: "100%",
     display: "flex",
     justifyContent: "space-between",
-    flexDirection: "row",
+    flexDirection: "column",
     alignContent: "space-between",
   },
 });
---
+++ ui/LineInput.jsx
@@ -0,0 +1,49 @@
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import { STYLES, THEME } from "../constants/constants";
+import { useEffect, useState } from "react";
+
+const LineInput = ({ inputLabel, placeholder, onChange }) => {
+  let [input, setInput] = useState("");
+
+  useEffect(() => {
+    onChange(input);
+  }, [input]);
+
+  return (
+    <View style={styles.inputField}>
+      <Text style={styles.label}>{inputLabel}</Text>
+      <TextInput
+        onChangeText={(text) => setInput(text)}
+        style={styles.input}
+        value={input}
+        placeholder={placeholder}
+        placeholderTextColor={styles.input.placeholderColor}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputField: {
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    width: "100%",
+  },
+
+  label: {
+    width: "100%",
+    textAlign: "left",
+    fontSize: THEME.fontSize2,
+    color: THEME.colorPrimary,
+  },
+
+  input: {
+    fontSize: THEME.fontSize3,
+    paddingHorizontal: 10,
+    paddingVertical: 3,
+    ...STYLES.borderBottom1,
+    placeholderColor: THEME.colorLightGray,
+  },
+});
+
+export default LineInput;
